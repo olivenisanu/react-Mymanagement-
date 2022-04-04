3e8c824e6729d47055313867e2dfeee884a05651
Nisanu <nisanu@naver.com>
2022-04-04 20:34:43 +0900

    My Create react Project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="gray-background">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Lets try to create masnagement
+d
+
+        </h2>
+    </div>
+  );
+}
+
+export default App;
